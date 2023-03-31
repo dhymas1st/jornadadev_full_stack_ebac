a4c955f0991c90f9acd011485911502d4f53732c
Edmilson Santos <edsouza.js@gmail.com>
2023-03-31 17:17:50 -0300

    "terceira aula"

---
+++ src/pages/components/footer/VideoFooter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./VideoFooter.css"
+
+function VideoFooter() {
+  return (
+    <div className='videoFooter'>VideoFooter</div>
+  )
+}
+
+export default VideoFooter
