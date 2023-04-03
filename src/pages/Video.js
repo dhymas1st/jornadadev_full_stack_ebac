6daf60ed10d0491dbe1098f42c485a10169e5cca
Edmilson Santos <edsouza.js@gmail.com>
2023-04-03 18:19:19 -0300

    aula 04

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react";
-import VideoFooter from "./components/footer/VideoFooter"
+import VideoFooter from "./components/footer/VideoFooter";
+import VideoSidebar from "./components/sidebar/VideoSidebar";
 import "./video.css";
 
-function Video() {
+function Video({ likes, messages, shares, name, description, music, url }) {
   const videoRef = useRef(null);
   const [play, setPlay] = useState(false);
 
@@ -23,10 +24,18 @@ function Video() {
         ref={videoRef}
         onClick={handleStart}
         loop
-        src="https://webapp-va.tiktok.com/161415bcd92a7ed0b8161d0d02007baf/64263421/tos-useast2a-pve-0068/oI6guG3TAGeE7CSIraqCC4Do5DoiQgelfkCSbn?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C0%7C0&br=1108&bt=554&cs=0&ds=3&ft=Ap6JtBiTq8ZmofAoCc_vjtnGqAhLrus&mime_type=video_mp4&qs=0&rc=NXZpTGRTaFBnKWZzZXpoaGRmOTk0OmY0aDo6Ozg5ZWg3PEBpajY6bzQ6Zjd3ajMzNzgzM0BTbGt2aXFEOmpoXitiK2p0OiM2YmAuNWJhXjExMzQyMV5iYSNebGwucjRvc2ZgLS1kLzZzcw%3D%3D&l=20230330191450E10B952914831C51DF31&btag=80000&ply_type=3&policy=eyJ2bSI6MywidWlkIjoiNjg5MzA1NTg4NTAyMDEyMDA2OSJ9"
+        src={ url }
       ></video>
-      {/* Side bar*/}
-      <VideoFooter />
+      <VideoSidebar
+        likes={likes}
+        messages={messages}
+        shares={shares}
+      />
+      <VideoFooter
+        name={name}
+        description={description}
+        music={music}
+      />
     </div>
   );
 }
--- src/pages/components/footer/VideoFooter.js
+++ src/pages/components/footer/VideoFooter.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import "./VideoFooter.css"
+import "./videoFooter.css"
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 
-function VideoFooter() {
-  return (
-    <div className='videoFooter'>VideoFooter</div>
-  )
+function VideoFooter({ name, description, music }) {
+    return (
+        <div className='videoFooter'>
+            <div className='videoFooter__text'>
+                <h3>@{ name }</h3>
+                <p>{ description }</p>
+                <div className='videoFooter__music'>
+                    <MusicNoteIcon className='videoFooter__icon' />
+                    <div className='videoFooterMusic__text'>
+                        <p>{ music }</p>
+                    </div>
+                </div>
+            </div>
+            <img
+                className='videoFooter___record'
+                src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png"
+                alt="Imagem de um vinil girando"
+            />
+        </div>
+    )
 }
 
 export default VideoFooter
